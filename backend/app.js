52c66b72e9bbaf530b47a7256869aefb2a93d2dc
Malaya Kumar Pradhan <malayakumarpradhan1507@gmail.com>
2026-03-21 06:45:58 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ const intializeDBServer = async () =>{
         await db.query(createUserTable);
 
         const createTasksTable = `
-            CREATE TABLE tasks (
+            CREATE TABLE IF NOT EXISTS tasks (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 title VARCHAR(255) NOT NULL,
                 description TEXT,
@@ -41,7 +41,7 @@ const intializeDBServer = async () =>{
                 user_id INT,
                 FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
                );
-        ';
+        `;
         await db.query(createTasksTable);
         
         console.log("Database tables checked and ready!");
